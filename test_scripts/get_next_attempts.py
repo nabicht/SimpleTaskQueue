ffa64875ba21d82ed428b0c6115be9e78ede05c9
Peter F. Nabicht <nabicht@gmail.com>
2018-09-07 19:16:28 -0500

    initial commit of a new restful test script

---
+++ test_scripts/get_next_attempts.py
@@ -0,0 +1,14 @@
+
+from simple_task_client import add_task
+from simple_task_client import get_next_attempt
+
+SERVER = 'http://localhost:5000/'
+
+task_1_id = add_task(SERVER, "cp some_files to_here/.", name="copy files", description="copying some stuff")
+task_2_id = add_task(SERVER, "cp some_files to_here/.", name="copy files again", description="more copying some stuff")
+task_3_id = add_task(SERVER, "python run_some_stuff.py", name="running python", description="running some python stuff because python runs on the command line", max_attempts=2)
+task_4_id = add_task(SERVER, "python run_different_stuff.py", name="running python", max_attempts=3, dependent_on=[task_1_id, task_3_id])
+task_5_id = add_task(SERVER, "grep blah big_ol.txt >> /mnt/remote_storage/smaller.txt", name="result filtering", duration=100.0)
+
+get_next_attempt(SERVER, "runner_a")
+
