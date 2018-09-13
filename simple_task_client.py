27d8eea70b35c743df6998b183f84ba942672ef1
Peter F. Nabicht <nabicht@gmail.com>
2018-09-13 13:17:50 -0500

    made runner_id configurable via command line. Defaults to using uuid.uuid1(). Closes #27

--- simple_task_client.py
+++ simple_task_client.py
@@ -14,7 +14,7 @@ def add_task(server, command, name=None, description=None, dependent_on=None, ma
     if name is not None:
         payload["name"] = str(name)
     if description is not None:
-        payload["description"]=str(description)
+        payload["description"] = str(description)
     if dependent_on is not None:
         dependent_on_list = []
         for d in dependent_on:
@@ -57,8 +57,8 @@ def get_next_attempt(server, runner_id):
     return json.loads(r.text)
 
 
-def main(server, wait_seconds, risky=False):
-    runner_id = uuid.uuid1()
+def main(server, wait_seconds, runner_id, risky=False):
+    print runner_id
     while True:
         attempt_info = get_next_attempt(server, runner_id)
         if attempt_info["status"] == "task":
@@ -77,6 +77,7 @@ def main(server, wait_seconds, risky=False):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    temp_runner_id = str(uuid.uuid1())
     parser.add_argument('-s', action="store", dest="server_url", required=True,
                         help="the url of the SimpleTaskServer")
     parser.add_argument("-risky", action="store_true", dest="risky", required=False,
@@ -84,5 +85,8 @@ if __name__ == "__main__":
     parser.add_argument("-wait_time", action="store", dest="wait_time", type=float, nargs='?',
                         const=5.0, default=5.0, required=False,
                         help="the number of seconds to wait after no attempts to run before querying server for a new attempt. Defaults to 5.")
+    parser.add_argument("-runner_id", action="store", dest="runner_id", nargs='?', const=temp_runner_id,
+                        default=temp_runner_id, required=False,
+                        help="The client's identifier. It should be unique across runners. If not defined, a unique id is randomly selected")
     args = parser.parse_args()
-    main(args.server_url, args.wait_time, risky=args.risky)
+    main(args.server_url, args.wait_time, args.runner_id, risky=args.risky)
