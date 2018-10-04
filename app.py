dccd217efb743df328137115ca3bd30038f7827d
Peter F. Nabicht <nabicht@gmail.com>
2018-10-03 20:57:13 -0500

    cleaned up code -- separated the primary script in file into methods and am now passing information around with proper arguments rather than relying on scope of the script, which should be more scope safe.

--- app.py
+++ app.py
@@ -40,63 +40,34 @@ class TaskIDCreator:
         return uuid.uuid1().hex
 
 
-errors = {
-    'UnknownDependencyException': {
-        'message': "One or more specified dependent_on Task IDs are unknown by the server. Task not added!",
-        'status': 400,
-    },
-}
-
-app = Flask(__name__)
-Bootstrap(app)
-api = Api(app, catch_all_404s=True, errors=errors)
-
-task_id_creator = TaskIDCreator()
-
-log_file_name = util.time_stamped_file_name("stq")
-logger = util.basic_logger(log_file_name=log_file_name, file_level=logging.DEBUG, console_level=logging.DEBUG)
-
-task_manager = TaskManager(logger)
-
-task_post_parser = reqparse.RequestParser()
-task_post_parser.add_argument('command', dest='command', required=True,
-                              help="what gets executed in the command line")
-task_post_parser.add_argument('name', dest='name', required=False,
-                              help='a brief, human readable name for the task (optional)')
-task_post_parser.add_argument('description', dest='description', required=False,
-                              help='a more in-depth description of the task (optional)')
-task_post_parser.add_argument('duration', dest='duration', required=False, type=float,
-                              help='how long, in seconds, the task should run before a new attempt is made. Microseconds are defined to the right of the decimal (optional, with default of no applied duration).')
-task_post_parser.add_argument('max_attempts', dest='max_attempts', required=False, type=int,
-                              help="The max amount of times you want to try to attempt to run the task (optional, with default of 1)")
-task_post_parser.add_argument('dependent_on', dest='dependent_on', required=False, action='append',
-                              help="the ID of a task that this task is dependent upon (optional, can be multiple).")
-
-task_delete_parser = reqparse.RequestParser()
-task_delete_parser.add_argument("task_id", dest="task_id", required=True, help="ID of Task to be deleted.")
-
-get_next_attempt = reqparse.RequestParser()
-get_next_attempt.add_argument('runner_id', dest='runner_id', required=True,
-                              help='The unique identifier of the runner.')
-
-attempt_update = reqparse.RequestParser()
-attempt_update.add_argument('runner_id', dest='runner_id', required=True, help='The unique identifier of the runner.')
-attempt_update.add_argument('task_id', dest='task_id', required=True, help='The unique identifier of the task being attempted.')
-attempt_update.add_argument('attempt_id', dest='attempt_id', required=True, help='The unique identifier of the attempt.')
-attempt_update.add_argument('status', dest='status', required=True, help='Status of attempt: "failed" or "completed".')
-attempt_update.add_argument('message', dest='message', required=False, help='Status of attempt: "failed" or "completed".')
-
-
 # a wrapper that creates a Resource to interact with TaskManager and does some JSON/restful specific stuff
 class TaskManagement(Resource):
 
     def __init__(self, **kwargs):
         self._logger = kwargs.get("logger")
+        self._task_id_creator = kwargs.get("task_id_creator")
+        self._task_manager = kwargs.get("task_manager")
+        self._task_post_parser = reqparse.RequestParser()
+        self._task_post_parser.add_argument('command', dest='command', required=True,
+                                            help="what gets executed in the command line")
+        self._task_post_parser.add_argument('name', dest='name', required=False,
+                                            help='a brief, human readable name for the task (optional)')
+        self._task_post_parser.add_argument('description', dest='description', required=False,
+                                            help='a more in-depth description of the task (optional)')
+        self._task_post_parser.add_argument('duration', dest='duration', required=False, type=float,
+                                            help='how long, in seconds, the task should run before a new attempt is made. Microseconds are defined to the right of the decimal (optional, with default of no applied duration).')
+        self._task_post_parser.add_argument('max_attempts', dest='max_attempts', required=False, type=int,
+                                            help="The max amount of times you want to try to attempt to run the task (optional, with default of 1)")
+        self._task_post_parser.add_argument('dependent_on', dest='dependent_on', required=False, action='append',
+                                            help="the ID of a task that this task is dependent upon (optional, can be multiple).")
+
+        self._task_delete_parser = reqparse.RequestParser()
+        self._task_delete_parser.add_argument("task_id", dest="task_id", required=True, help="ID of Task to be deleted.")
 
     def post(self):
-        args = task_post_parser.parse_args()
+        args = self._task_post_parser.parse_args()
         self._logger.info("TaskManagement.post: %s" % str(args))
-        task = Task(task_id_creator.id(),
+        task = Task(self._task_id_creator.id(),
                     args.command,
                     datetime.now(),
                     name=args.name if args.name is not None else "",
@@ -104,13 +75,13 @@ class TaskManagement(Resource):
                     duration=args.duration,
                     max_attempts=args.max_attempts if args.max_attempts is not None else 1,
                     dependent_on=args.dependent_on)
-        task_manager.add_task(task)
+        self._task_manager.add_task(task)
         return task.to_json(), 201
 
     def delete(self):
-        args = task_delete_parser.parse_args()
+        args = self._task_delete_parser.parse_args()
         self._logger.info("TaskManagement.delete: %s" % str(args))
-        deleted = task_manager.delete_task(args.task_id)
+        deleted = self._task_manager.delete_task(args.task_id)
         if deleted:
             return {"status": "task deleted", "task_id": args.task_id}, 200
         else:
@@ -123,6 +94,21 @@ class AttemptManagement(Resource):
 
     def __init__(self, **kwargs):
         self._logger = kwargs.get("logger")
+        self._task_manager = kwargs.get("task_manager")
+        self._get_next_attempt = reqparse.RequestParser()
+        self._get_next_attempt.add_argument('runner_id', dest='runner_id', required=True,
+                                            help='The unique identifier of the runner.')
+        self._attempt_update = reqparse.RequestParser()
+        self._attempt_update.add_argument('runner_id', dest='runner_id', required=True,
+                                          help='The unique identifier of the runner.')
+        self._attempt_update.add_argument('task_id', dest='task_id', required=True,
+                                          help='The unique identifier of the task being attempted.')
+        self._attempt_update.add_argument('attempt_id', dest='attempt_id', required=True,
+                                          help='The unique identifier of the attempt.')
+        self._attempt_update.add_argument('status', dest='status', required=True,
+                                          help='Status of attempt: "failed" or "completed".')
+        self._attempt_update.add_argument('message', dest='message', required=False,
+                                          help='Status of attempt: "failed" or "completed".')
 
     @staticmethod
     def _task_attempt_json(task, attempt):
@@ -132,10 +118,10 @@ class AttemptManagement(Resource):
                 'attempt_id': attempt.id()}
 
     def get(self):
-        args = get_next_attempt.parse_args()
+        args = self._get_next_attempt.parse_args()
         self._logger.info("AttemptManagement.get: %s" % str(args))
         current_time = datetime.now()
-        task, attempt = task_manager.start_next_attempt(args.runner_id, current_time)
+        task, attempt = self._task_manager.start_next_attempt(args.runner_id, current_time)
         if task is not None:
             json_dict = self._task_attempt_json(task, attempt)
             self._logger.info("AttemptManagement.get next attempt: %s" % str(json_dict))
@@ -146,15 +132,15 @@ class AttemptManagement(Resource):
             return json_dict, 200
 
     def put(self):
-        args = attempt_update.parse_args()
+        args = self._attempt_update.parse_args()
         self._logger.info("AttemptManagement.put: %s" % str(args))
         status = args.status.lower()
         if status == "failed":
-            task_manager.fail_attempt(args.task_id, args.attempt_id, "client reported")
+            self._task_manager.fail_attempt(args.task_id, args.attempt_id, "client reported")
         elif status == "completed":
-            task_manager.complete_attempt(args.task_id, args.attempt_id, datetime.now())
+            self._task_manager.complete_attempt(args.task_id, args.attempt_id, datetime.now())
         else:
-            task_manager.fail_attempt(args.task_id, args.attempt_id, "unknown status reported")
+            self._task_manager.fail_attempt(args.task_id, args.attempt_id, "unknown status reported")
             # TODO log this
             return {"message": "%s is an unknown status. Should be 'completed' or 'failed'. Falling back to failed." % args.status}, 400
 
@@ -163,6 +149,7 @@ class MonitorTasks(Resource):
 
     def __init__(self, **kwargs):
         self._logger = kwargs.get("logger")
+        self._task_manager = kwargs.get("task_manager")
 
     @staticmethod
     def _dependent_on_str(dependent_ons):
@@ -176,7 +163,7 @@ class MonitorTasks(Resource):
         self._logger.info("MonitorTasks.get: %s" % list_type)
         list_of_tasks = []
         if list_type.lower() == "todo":
-            to_do = task_manager.todo_tasks()
+            to_do = self._task_manager.todo_tasks()
             for task in to_do:
                 d = {"task_id": task.task_id(),
                      "status": "To Do",
@@ -190,7 +177,7 @@ class MonitorTasks(Resource):
                      }
                 list_of_tasks.append(d)
         elif list_type.lower() == "inprocess":
-            in_process = task_manager.in_process_tasks()
+            in_process = self._task_manager.in_process_tasks()
             for task in in_process:
                 current_runner = ""
                 if task.most_recent_attempt().is_in_process():
@@ -211,7 +198,7 @@ class MonitorTasks(Resource):
                      }
                 list_of_tasks.append(d)
         elif list_type.lower() == "failed":
-            done = task_manager.done_tasks()
+            done = self._task_manager.done_tasks()
             for task in done:
                 if task.is_failed():
                     d = {"task_id": task.task_id(),
@@ -220,12 +207,12 @@ class MonitorTasks(Resource):
                          "name": task.name,
                          "description": task.desc,
                          "command": task.cmd,
-                         "dependencies": self._dependent_on_str(task_manager.dependencies(task.task_id())),
+                         "dependencies": self._dependent_on_str(self._task_manager.dependencies(task.task_id())),
                          "attempts": task.num_attempts()
                          }
                     list_of_tasks.append(d)
         elif list_type.lower() == "completed":
-            done = task_manager.done_tasks()
+            done = self._task_manager.done_tasks()
             for task in done:
                 if task.is_completed():
                     d = {"task_id": task.task_id(),
@@ -235,7 +222,7 @@ class MonitorTasks(Resource):
                          "name": task.name,
                          "description": task.desc,
                          "command": task.cmd,
-                         "dependencies": self._dependent_on_str(task_manager.dependencies(task.task_id())),
+                         "dependencies": self._dependent_on_str(self._task_manager.dependencies(task.task_id())),
                          "attempts": task.num_attempts()
                          }
                     list_of_tasks.append(d)
@@ -244,14 +231,34 @@ class MonitorTasks(Resource):
         return {"data": list_of_tasks}, 200
 
 
-api.add_resource(TaskManagement, '/task', resource_class_kwargs={'logger': logger})
-api.add_resource(AttemptManagement, '/attempt', resource_class_kwargs={'logger': logger})
-api.add_resource(MonitorTasks, '/listtasks/<list_type>', resource_class_kwargs={'logger': logger})
+def create_app():
+    errors = {
+        'UnknownDependencyException': {
+            'message': "One or more specified dependent_on Task IDs are unknown by the server. Task not added!",
+            'status': 400,
+        },
+    }
 
+    app = Flask(__name__)
+    Bootstrap(app)
+    api = Api(app, catch_all_404s=True, errors=errors)
 
-@app.route('/')
-def task_queue_overview():
-    return render_template('overview.html')
+    task_id_creator = TaskIDCreator()
+
+    log_file_name = util.time_stamped_file_name("stq")
+    logger = util.basic_logger(log_file_name=log_file_name, file_level=logging.DEBUG, console_level=logging.DEBUG)
+
+    task_manager = TaskManager(logger)
+
+    api.add_resource(TaskManagement, '/task', resource_class_kwargs={'logger': logger, 'task_manager': task_manager, 'task_id_creator': task_id_creator})
+    api.add_resource(AttemptManagement, '/attempt', resource_class_kwargs={'logger': logger, 'task_manager': task_manager})
+    api.add_resource(MonitorTasks, '/listtasks/<list_type>', resource_class_kwargs={'logger': logger, "task_manager": task_manager})
+
+    @app.route('/')
+    def task_queue_overview():
+        return render_template('overview.html')
+
+    return app
 
 
 if __name__ == '__main__':
@@ -263,6 +270,7 @@ if __name__ == '__main__':
                         default=[5000], required=False,
                         help="The port. Defaults to 5000")
     cmd_args = parser.parse_args()
+    app = create_app()
     app.run(host=cmd_args.host[0], port=cmd_args.port[0], threaded=False)
 
 
